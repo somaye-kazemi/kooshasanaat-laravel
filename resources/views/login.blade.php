82896629c8ee7702da5fcc6cb293035cb58c6d88
somaye-kazemi <somkaz68@gmail.com>
2022-09-07 07:59:15 -0700

    crad shop

---
+++ resources/views/login.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>بورداش - قالب داشبرد ادمین</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/media/image/favicon.png"/>
+
+    <!-- Plugin styles -->
+    <link rel="stylesheet" href="vendors/bundle.css" type="text/css">
+
+    <!-- App styles -->
+    <link rel="stylesheet" href="assets/css/app.min.css" type="text/css">
+</head>
+<body class="form-membership">
+
+<!-- begin::preloader-->
+<div class="preloader">
+    <div class="preloader-icon"></div>
+</div>
+<!-- end::preloader -->
+
+<div class="form-wrapper">
+
+    <!-- logo -->
+    <div id="logo">
+        <img class="logo" src="assets/media/image/logo-F.png" alt="image">
+        <img class="logo-dark" src="assets/media/image/logo-dark.html" alt="image">
+    </div>
+    <!-- ./ logo -->
+
+    <h5>ورود</h5>
+
+    <!-- form -->
+    <form>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="نام کاربری یا ایمیل" required autofocus>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="رمزعبور" required>
+        </div>
+        <div class="form-group d-flex justify-content-between">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" checked="" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">مرا به خاطر بسپار</label>
+            </div>
+            <a href="recover-password.html">بازگردانی رمزعبور</a>
+        </div>
+        <button class="btn btn-primary btn-block">ورود</button>
+        <hr>
+        <p class="text-muted">ورود با استفاده از شبکه‌های اجتماعی</p>
+        <ul class="list-inline">
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-facebook">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-twitter">
+                    <i class="fa fa-twitter"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-dribbble">
+                    <i class="fa fa-dribbble"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-linkedin">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-google">
+                    <i class="fa fa-google"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-behance">
+                    <i class="fa fa-behance"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="btn btn-floating btn-instagram">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </li>
+        </ul>
+        <hr>
+        <p class="text-muted">اکانت ندارید?</p>
+        <a href="register.html" class="btn btn-outline-light btn-sm">ثبت نام کنید!</a>
+    </form>
+    <!-- ./ form -->
+
+</div>
+
+<!-- Plugin scripts -->
+<script src="vendors/bundle.js"></script>
+
+<!-- App scripts -->
+<script src="assets/js/app.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>بورداش - قالب داشبرد ادمین</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/media/image/favicon.png"/>
+
+    <!-- Plugin styles -->
+    <link rel="stylesheet" href="vendors/bundle.css" type="text/css">
+
+    <!-- App styles -->
+    <link rel="stylesheet" href="assets/css/app.min.css" type="text/css">
+</head>
+<body class="form-membership">
+
+<!-- begin::preloader-->
+<div class="preloader">
+    <div class="preloader-icon"></div>
+</div>
+<!-- end::preloader -->
+
+<div class="form-wrapper">
+
+    <!-- logo -->
+    <div id="logo">
+        <img class="logo" src="assets/media/image/logo-F.png" alt="image">
+        <img class="logo-dark" src="assets/media/image/logo-dark.html" alt="image">
+    </div>
+    <!-- ./ logo -->
+
+    <h5>ساخت اکانت</h5>
+
+    <!-- form -->
+    <form>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="نام" required autofocus>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="نام خانوادگی" required>
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" placeholder="ایمیل " required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="رمزعبور" required>
+        </div>
+        <button class="btn btn-primary btn-block">ثبت‌نام</button>
+        <hr>
+        <p class="text-muted">اکانت دارید?</p>
+        <a href="login.blade.php" class="btn btn-outline-light btn-sm">ثبت نام!</a>
+    </form>
+    <!-- ./ form -->
+
+</div>
+
+<!-- Plugin scripts -->
+<script src="vendors/bundle.js"></script>
+
+<!-- App scripts -->
+<script src="assets/js/app.min.js"></script>
+</body>
+
+</html>
